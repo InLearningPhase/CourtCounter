298d515af996f495f83cb967ee8b517966c63d13
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-02-24 22:05:10 +0530

    Add another team TeamB

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +22,39 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
     public void addThreeForTeamA(View view) {
-        score += 3;
-        displayForTeamA(score);
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addTwoForTeamA(View view) {
-        score += 2;
-        displayForTeamA(score);
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void freeThrow(View view) {
-        score += 1;
-        displayForTeamA(score);
+    public void freeThrowTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
+
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
 }
