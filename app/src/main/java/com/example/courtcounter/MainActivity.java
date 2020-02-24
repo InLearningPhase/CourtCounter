e33027e4af83351b721fb6feb001dc9fdee5cce8
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-02-24 21:47:21 +0530

    Add functionality to buttons

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View view) {
+        score += 3;
+        displayForTeamA(score);
+    }
+
+    public void addTwoForTeamA(View view) {
+        score += 2;
+        displayForTeamA(score);
+    }
+
+    public void freeThrow(View view) {
+        score += 1;
+        displayForTeamA(score);
     }
 }
