cff05db5270cb84b6b27a15ed64660cb3587d827
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-02-25 20:26:57 +0530

    final commit

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+   public int scoreTeamA = 0;
+   public int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
